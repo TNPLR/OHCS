838e0cfdf001d0910bd8a032f83bd7e3e617c779
TNPLR <30339558+TNPLR@users.noreply.github.com>
2018-02-16 18:57:01 +0800

    Debug mode bug fixed

--- src/OCSS.cpp
+++ src/OCSS.cpp
@@ -50,7 +50,9 @@ void Encrypt(string& Data,vector<char>& Key)
 #endif
 	}
         ToBase(Data, last_key, 95);
-        printf("Base %d-\"%c\"||", 95, Char_List[94]); cout<<Data<<'\n';
+#ifdef DEBUG
+	printf("Base %d-\"%c\"||", 95, Char_List[94]); cout<<Data<<'\n';
+#endif
 	return;
 }
 void Decrypt(string& Data,vector<char>& Key)
@@ -91,7 +93,9 @@ void Decrypt(string& Data,vector<char>& Key)
                 last_key = remove_value;
         }
         ToBase(Data, last_key, 95);
+#ifdef DEBUG
 	printf("Base %d-\"%c\"||", 95, Char_List[94]); cout<<Data<<'\n';
+#endif
 }
 void ToBase(string& Data,int Original_Base,int New_Base)
 {
