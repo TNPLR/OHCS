0783df94d68776a93aca0ff6b6eeda6b0635bd86
TNPLR <hsiaosvideo@gmail.com>
2020-02-09 12:42:59 +0800

    del.cpp: Remove unused code.

--- src/del.cpp
+++ src/del.cpp
@@ -1,27 +1,6 @@
 #include "del.hpp"
 #include <string>
 #include <string.h>
-/*
-void dell::del(char *src, char remove_key)
-{
-	char *fp = src;
-	while (*src) {
-		if (*src != remove_key) {
-			*fp = *src;
-			fp++;
-		}
-		src++;
-	}
-	*fp = '\0' ;
-}
-void dell::del(std::string &str, char remove_key)
-{
-	char tmp[str.size()+1];
-	memcpy(&tmp, str.c_str(), str.size() + 1);
-	del(tmp, remove_key);
-	str = tmp;
-}
-*/
 void dell::del(std::string &str, char remove_key)
 {
 	auto copy = str.begin();
